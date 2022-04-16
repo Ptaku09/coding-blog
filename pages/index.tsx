438373c25dfa813f43bde716c5537185fc350fcd
Mateusz Ptak <ptakm02@gmail.com>
2022-04-16 21:44:25 +0200

    homepage background

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,45 +5,14 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Next-app boilerplate</title>
+        <title>Coding blog</title>
       </Head>
 
-      <div className="w-screen h-screen flex items-center justify-center flex-col">
-        <h1 className="absolute top-5 mx-auto text-5xl text-sky-700 border-b-2 border-b-sky-700 pb-5 px-7">Create-next-app boilerplate</h1>
-        <p className="text-4xl text-left mb-3">Contains:</p>
-        <ul className="list-disc text-lg">
-          {[
-            {
-              name: 'Typescript',
-              link: 'https://www.typescriptlang.org/',
-            },
-            {
-              name: 'Tailwind',
-              link: 'https://tailwindcss.com/',
-            },
-            {
-              name: 'Prettier',
-              link: 'https://prettier.io/',
-            },
-            {
-              name: 'ESLint',
-              link: 'https://eslint.org/',
-            },
-            {
-              name: 'Husky',
-              link: 'https://typicode.github.io/husky/#/',
-            },
-          ].map(({ name, link }: { name: string; link: string }) => {
-            return (
-              <li key={name}>
-                <a className="hover:border-b-2 hover:border-b-blue-500 hover:text-blue-500" href={link} target="_blank" rel="noreferrer">
-                  {name}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
-        <p className="text-lg mt-3">...and more!</p>
+      <div className="w-screen h-screen relative overflow-hidden">
+        <div className="w-96 h-96 md:w-[32rem] md:h-[32rem] absolute -top-20 lg:top-20 -left-24 bg-blue-700 rounded-full blur-2xl" />
+        <div className="w-72 h-72 md:w-[36rem] md:h-[36rem] lg:w-96 lg:h-96 absolute -bottom-32 lg:-top-20 lg:-right-24 bg-blue-700 rounded-full blur-2xl" />
+        <div className="w-80 h-80 md:w-[40rem] md:h-[40rem] absolute -right-32 top-72 lg:-bottom-56 md:left-[35rem] bg-purple-700 rounded-full blur-2xl" />
+        <div className="w-full h-full bg-white z-[2] absolute opacity-80" />
       </div>
     </>
   );
