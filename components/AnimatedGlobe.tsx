db597910f3c9891032b28d5a8615f806ef18b387
Mateusz Ptak <ptakm02@gmail.com>
2022-05-05 19:27:54 +0200

    add arcs to globe

--- components/AnimatedGlobe.tsx
+++ components/AnimatedGlobe.tsx
@@ -3,9 +3,17 @@ import * as THREE from 'three';
 
 const Globe = typeof window !== 'undefined' ? require('react-globe.gl').default : () => null;
 
+type arcsData = {
+  startLat: number;
+  startLng: number;
+  endLat: number;
+  endLng: number;
+};
+
 const AnimatedGlobe = () => {
   const [countries, setCountries] = useState({ features: [] });
-  const [globeMaterial, setGlobeMaterial] = useState(null as THREE.MeshPhongMaterial | null);
+  const [globeMaterial, setGlobeMaterial] = useState(new THREE.MeshPhongMaterial());
+  const [arcs, setArcs] = useState([] as arcsData[]);
   const globeRef = useRef() as any;
 
   useEffect(() => {
@@ -18,28 +26,40 @@ const AnimatedGlobe = () => {
     globeRef.current.controls().enableZoom = false;
     globeRef.current.controls().autoRotate = true;
     globeRef.current.controls().autoRotateSpeed = 0.65;
-    globeRef.current.camera().fov = 70;
-
+    globeRef.current.camera().fov = 60;
+    globeRef.current.camera().position.set(globeRef.current.camera().position.x, 100, globeRef.current.camera().position.z);
     setGlobeMaterial(new THREE.MeshPhongMaterial({ color: 0x474ed6, shininess: 0.5 }));
 
-    // change light position
-    setTimeout(() => {
-      const directionalLight = globeRef.current.scene().children.find((obj3d: any) => obj3d.type === 'DirectionalLight');
-      directionalLight && directionalLight.position.set(-3, 1.5, 1);
-    });
+    // set arcs
+    const arcs = [] as arcsData[];
+    for (let i = 0; i < 15; i++)
+      arcs.push({
+        startLat: (Math.random() - 0.5) * 180,
+        startLng: (Math.random() - 0.5) * 360,
+        endLat: (Math.random() - 0.5) * 180,
+        endLng: (Math.random() - 0.5) * 360,
+      });
+
+    setArcs(arcs);
   }, []);
 
   return (
     <Globe
       animateIn={false}
-      backgroundColor="rgba(0, 0, 0, 0)"
+      backgroundColor="rgba(0, 0, 0, 1)"
       globeMaterial={globeMaterial}
       hexPolygonsData={countries.features}
       hexPolygonResolution={3}
       hexPolygonMargin={0.7}
       hexPolygonColor={() => 0xffffff}
       atmosphereColor={'#a5d6fe'}
-      atmosphereAltitude={0.25}
+      atmosphereAltitude={0.3}
+      arcsData={arcs}
+      arcStroke={0.8}
+      arcColor={() => '#e967ba'}
+      arcDashLength={1}
+      arcDashGap={0.05}
+      arcDashAnimateTime={5000}
       ref={globeRef}
     />
   );
