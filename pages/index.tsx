a0c39ccdd048791674b1dc6413da3d05cc15937b
Mateusz Ptak <ptakm02@gmail.com>
2022-05-13 22:07:34 +0200

    address bar on mobile - fix 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,10 +24,10 @@ const Home: NextPage = () => {
         />
       </Head>
 
-      <div id="main-wrapper" className="w-screen min-h-full h-auto">
+      <div className="w-screen h-screen">
         <MobileHomeBackground />
         <div className="w-screen h-full relative flex items-start justify-start">
-          <div className="absolute z-[2] w-screen h-2/5">
+          <div className="absolute z-[2] w-screen h-1/2">
             <div className="w-screen relative p-4 pr-8 text-2xl font-raleway font-bold text-white flex items-center justify-between">
               <LogoAndName />
               <FontAwesomeIcon icon={faLightbulb} />
@@ -40,7 +40,7 @@ const Home: NextPage = () => {
           <div className="mt-[18rem]">
             <AnimatedGlobe />
           </div>
-          <div className="w-[102rem] h-[102rem] absolute top-[49.5rem] left-1/2 -translate-x-1/2 bg-white rounded-full flex items-center justify-start flex-col">
+          <div className="w-screen h-[102rem] absolute top-[49.5rem] left-1/2 -translate-x-1/2 bg-white rounded-full flex items-center justify-start flex-col">
             <a className="mt-3" href="#how-to-start">
               <FontAwesomeIcon className="animate-myBounce" icon={faArrowDown} />
             </a>
