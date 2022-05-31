2854e0dce128bee6ba66a3268cc69499583851ea
Mateusz Ptak <ptakm02@gmail.com>
2022-06-01 00:44:41 +0200

    add animations to desktop version of hts and create left and right hts component

---
+++ components/atoms/DesktopHowToStartComponentLeft.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+type Props = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const DesktopHowToStartComponentLeft = ({ title, children }: Props) => {
+  return (
+    <div className="w-auto h-auto relative">
+      <div
+        id={title.split(' ').join('-').toLowerCase()}
+        className="w-auto h-auto relative bg-gray-100 shadow-2xl shadow-blue-500/40 py-5 px-10 flex items-center justify-center flex-col gap-5"
+      >
+        <p className="font-bebas text-4xl border-b-2 border-b-gray-300 px-4 pb-2">{title}</p>
+        {children}
+      </div>
+      <div id={`${title.split(' ').join('-').toLowerCase()}-bg`} className="w-full h-full absolute -z-10 top-5 right-5 bg-purple-600" />
+    </div>
+  );
+};
+
+export default DesktopHowToStartComponentLeft;
--- components/organisms/DesktopHowToStartSection.tsx
+++ components/organisms/DesktopHowToStartSection.tsx
@@ -2,50 +2,174 @@ import CreateAccount from '../../public/images/create-acc.svg';
 import WaitForReactions from '../../public/images/wait-for-reactions.svg';
 import ShareYourCode from '../../public/images/share-your-code.svg';
 import Image from 'next/image';
-import DesktopHowToStartComponent from '../atoms/DesktopHowToStartComponent';
+import DesktopHowToStartComponentLeft from '../atoms/DesktopHowToStartComponentLeft';
 import NewPostBlack from '../../public/icons/new-post-black.svg';
-import React from 'react';
-
-enum componentType {
-  LEFT = 'left',
-  RIGHT = 'right',
-}
+import React, { RefObject, useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import DesktopHowToStartComponentRight from '../atoms/DesktopHowToStartComponentRight';
 
 const DesktopHowToStartSection = () => {
+  const ref = useRef() as RefObject<HTMLElement>;
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const element = ref.current;
+
+    gsap.fromTo(
+      element!.querySelector('#create-an-account-bg'),
+      {
+        x: -70,
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        lazy: false,
+        scrollTrigger: {
+          scroller: 'body',
+          trigger: element!.querySelector('#create-an-account-bg'),
+          start: 'top bottom-=200',
+          end: 'top bottom-=200',
+        },
+      }
+    );
+
+    gsap.fromTo(
+      element!.querySelector('#create-an-account'),
+      {
+        x: 70,
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        lazy: false,
+        scrollTrigger: {
+          scroller: 'body',
+          trigger: element!.querySelector('#create-an-account'),
+          start: 'top bottom-=250',
+          end: 'top bottom-=250',
+        },
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const element = ref.current;
+
+    gsap.fromTo(
+      element!.querySelector('#share-your-code-bg'),
+      {
+        x: 70,
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        lazy: false,
+        scrollTrigger: {
+          scroller: 'body',
+          trigger: element!.querySelector('#share-your-code-bg'),
+          start: 'top bottom-=200',
+          end: 'top bottom-=200',
+        },
+      }
+    );
+
+    gsap.fromTo(
+      element!.querySelector('#share-your-code'),
+      {
+        x: -70,
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        lazy: false,
+        scrollTrigger: {
+          scroller: 'body',
+          trigger: element!.querySelector('#share-your-code'),
+          start: 'top bottom-=250',
+          end: 'top bottom-=250',
+        },
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const element = ref.current;
+
+    gsap.fromTo(
+      element!.querySelector('#wait-for-reactions-bg'),
+      {
+        x: -70,
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        lazy: false,
+        scrollTrigger: {
+          scroller: 'body',
+          trigger: element!.querySelector('#wait-for-reactions-bg'),
+          start: 'top bottom-=200',
+          end: 'top bottom-=200',
+        },
+      }
+    );
+
+    gsap.fromTo(
+      element!.querySelector('#wait-for-reactions'),
+      {
+        x: 70,
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        lazy: false,
+        scrollTrigger: {
+          scroller: 'body',
+          trigger: element!.querySelector('#wait-for-reactions'),
+          start: 'top bottom-=250',
+          end: 'top bottom-=250',
+        },
+      }
+    );
+  }, []);
+
   return (
-    <div className="h-auto w-screen grid grid-rows-3 pt-10 pb-20">
+    <section className="h-auto w-screen grid grid-rows-3 pt-10 pb-20" ref={ref}>
       <div className="w-1/2 h-auto flex items-center justify-end p-10">
-        <DesktopHowToStartComponent title="Create an account">
+        <DesktopHowToStartComponentLeft title="Create an account">
           <p className="font-raleway w-80 text-center">Sign in for free via Google, Github or Twitter</p>
           <a href="https://storyset.com/technology" target="_blank" rel="noreferrer">
             <Image src={CreateAccount} height={250} width={250} alt="Create Account" />
           </a>
-          <div className="w-full h-full absolute -z-10 top-5 right-5 bg-purple-600" />
-        </DesktopHowToStartComponent>
+        </DesktopHowToStartComponentLeft>
       </div>
       <div className="w-screen h-auto flex justify-end">
         <div className="w-1/2 h-auto flex items-center justify-start p-10">
-          <DesktopHowToStartComponent title="Share your code">
+          <DesktopHowToStartComponentRight title="Share your code">
             <p className="font-raleway w-80 text-center">
               Click the <Image src={NewPostBlack} width={13} height={13} alt="new post" /> button and add your code with short comment.
             </p>
             <a href="https://storyset.com/technology" target="_blank" rel="noreferrer">
               <Image src={ShareYourCode} height={250} width={250} alt="Create Account" />
             </a>
-            <div className="w-full h-full absolute -z-10 top-5 left-5 bg-purple-600" />
-          </DesktopHowToStartComponent>
+          </DesktopHowToStartComponentRight>
         </div>
       </div>
       <div className="w-1/2 h-auto flex items-center justify-end p-10">
-        <DesktopHowToStartComponent title="Wait for reactions">
+        <DesktopHowToStartComponentLeft title="Wait for reactions">
           <p className="font-raleway w-80 text-center">Share your knowledge and gain reactions!</p>
           <a href="https://storyset.com/technology" target="_blank" rel="noreferrer">
             <Image src={WaitForReactions} height={250} width={250} alt="Create Account" />
           </a>
-          <div className="w-full h-full absolute -z-10 top-5 right-5 bg-purple-600" />
-        </DesktopHowToStartComponent>
+        </DesktopHowToStartComponentLeft>
       </div>
-    </div>
+    </section>
   );
 };
 
