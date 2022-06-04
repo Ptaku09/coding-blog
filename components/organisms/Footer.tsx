112d119f261e58928d7e81064354958f4bb84ffc
Mateusz Ptak <ptakm02@gmail.com>
2022-06-04 23:47:39 +0200

    change footer

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -6,34 +6,25 @@ import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <div className="w-screen relative z-[2] p-3 min-h-56 h-auto bg-black text-white grid grid-rows-[1fr_4fr_1fr] font-bebas">
-      <div className="flex justify-start text-xl border-b-[1px] pb-2">
+    <div className="w-screen relative z-[2] p-3 pb-0 min-h-56 h-auto bg-black text-white grid grid-rows-[1fr_2fr_1fr] font-bebas md:w-96 md:mx-auto">
+      <div className="flex justify-start text-xl border-b-[1px] md:border-0 pb-0">
         <LogoAndName />
       </div>
-      <div className="grid grid-cols-2 md:flex md:items-center md:justify-center md:gap-56 md:bg-black pt-5 px-5">
-        <div className="flex flex-col justify-start items-start gap-4">
-          <p className="text-xl">Follow me!</p>
-          <a className="flex items-center justify-center gap-1" href="https://github.com/Ptaku09" target="_blank" rel="noreferrer">
-            <Image src={GithubWhite} width={22} height={22} alt="github" />
-            <p className="text-sm">@Ptaku09</p>
-          </a>
-          <a className="flex items-center justify-center gap-1" href="https://twitter.com/Ptaku09" target="_blank" rel="noreferrer">
-            <Image src={TwitterWhite} width={20} height={20} alt="twitter" />
-            <p className="text-sm">@Ptaku09</p>
-          </a>
-          <a className="flex items-center justify-center gap-1" href="https://www.instagram.com/ptaku08" target="_blank" rel="noreferrer">
-            <Image src={InstagramWhite} width={20} height={20} alt="twitter" />
-            <p className="text-sm">@Ptaku08</p>
-          </a>
-        </div>
-        <div className="flex flex-col justify-start items-start gap-4 pl-5">
-          <p className="text-xl">About</p>
-          <p className="text-sm">Privacy</p>
-          <p className="text-sm">Policy</p>
-          <p className="text-sm">Terms</p>
-        </div>
+      <div className="flex justify-center items-center md:justify-start gap-8">
+        <a className="flex items-center justify-center gap-1" href="https://github.com/Ptaku09" target="_blank" rel="noreferrer">
+          <Image src={GithubWhite} width={22} height={22} alt="github" />
+          <p className="text-sm">@Ptaku09</p>
+        </a>
+        <a className="flex items-center justify-center gap-1" href="https://twitter.com/Ptaku09" target="_blank" rel="noreferrer">
+          <Image src={TwitterWhite} width={20} height={20} alt="twitter" />
+          <p className="text-sm">@Ptaku09</p>
+        </a>
+        <a className="flex items-center justify-center gap-1" href="https://www.instagram.com/ptaku08" target="_blank" rel="noreferrer">
+          <Image src={InstagramWhite} width={20} height={20} alt="twitter" />
+          <p className="text-sm">@Ptaku08</p>
+        </a>
       </div>
-      <div className="flex items-end justify-between w-full text-sm">
+      <div className="flex items-center justify-between w-full text-sm">
         <p>
           <span className="after:content-['❤'] after:mx-1">Created with</span>
           by{' '}
