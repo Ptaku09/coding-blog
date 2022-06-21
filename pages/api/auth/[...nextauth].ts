4b041092fcbc5d7f4143c7ab725ae7b57102f895
Mateusz Ptak <ptakm02@gmail.com>
2022-06-21 23:36:41 +0200

    configure sign in providers and swap dynamoDB with mongoDB adapter

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,29 +1,12 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
-import { DynamoDB, DynamoDBClientConfig } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
-import { DynamoDBAdapter } from '@next-auth/dynamodb-adapter';
 import Twitter from 'next-auth/providers/twitter';
 import Google from 'next-auth/providers/google';
-
-const config: DynamoDBClientConfig = {
-  credentials: {
-    accessKeyId: process.env.NEXT_AUTH_AWS_ACCESS_KEY_ID as string,
-    secretAccessKey: process.env.NEXT_AUTH_AWS_SECRET_ACCESS_KEY as string,
-  },
-  region: process.env.NEXT_AUTH_AWS_REGION as string,
-};
-
-const client = DynamoDBDocument.from(new DynamoDB(config), {
-  marshallOptions: {
-    convertEmptyValues: true,
-    removeUndefinedValues: true,
-    convertClassInstanceToMap: true,
-  },
-});
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import clientPromise from '../../../mongodb';
 
 export default NextAuth({
-  adapter: DynamoDBAdapter(client, { tableName: 'coding-blog-users' }),
+  adapter: MongoDBAdapter(clientPromise),
   secret: process.env.NEXT_AUTH_SECRET as string,
   providers: [
     GitHub({
