01e534c11b4614b041553743df0c1370d641a917
Mateusz Ptak <ptakm02@gmail.com>
2022-07-03 17:07:44 +0200

    fix progress bar dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,15 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppPropsWith
   return (
     <ThemeProvider attribute="class">
       <SessionProvider session={session} refetchInterval={0}>
-        <NextNProgress color="#9333ea" startPosition={0.2} stopDelayMs={200} height={5} showOnShallow={false} />
+        <NextNProgress
+          color="#9333ea"
+          startPosition={0.2}
+          stopDelayMs={200}
+          height={5}
+          showOnShallow={false}
+          options={{ showSpinner: false }}
+          nonce={undefined}
+        />
         {getLayout(<Component {...pageProps} />)}
       </SessionProvider>
     </ThemeProvider>
