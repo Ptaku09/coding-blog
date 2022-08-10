117c2f48ff9764ebdcc1120e1ee907228a769143
Mateusz Ptak <ptakm02@gmail.com>
2022-08-10 16:45:29 +0200

    board - add more breakpoints

--- components/templates/BoardLayout.tsx
+++ components/templates/BoardLayout.tsx
@@ -7,12 +7,13 @@ import ReloadBoard from '../atoms/ReloadBoard';
 import { useResizeDetector } from 'react-resize-detector';
 import BoardMenu from '../organisms/BoardMenu';
 import SearchMenu from '../organisms/SearchMenu';
+import CompactBoardMenu from '../organisms/CompactBoardMenu';
 
 const BoardLayout = ({ children }: { children: ReactNode }) => {
   const { width, ref } = useResizeDetector();
   return (
     <div ref={ref}>
-      {(width as number) < 890 ? (
+      {(width as number) < 770 ? (
         <>
           <MobileHeader />
           <PopupTutorial />
@@ -21,13 +22,26 @@ const BoardLayout = ({ children }: { children: ReactNode }) => {
           <ReloadBoard />
           <MobileFooter />
         </>
-      ) : (width as number) < 1226 ? (
-        <div className="grid grid-cols-[1fr_650px] bg-white dark:bg-dark-user">
-          <BoardMenu />
+      ) : (width as number) < 1050 ? (
+        <div className="grid grid-cols-[1fr_80px_650px_1fr] bg-white dark:bg-dark-user">
+          <div />
+          <CompactBoardMenu />
           <div className="w-full relative">
             <main className="border-x-[1px] dark:border-gray-500">{children}</main>
             <ReloadBoard />
           </div>
+          <div />
+        </div>
+      ) : (width as number) < 1226 ? (
+        <div className="grid grid-cols-[0.3fr_80px_650px_1fr_1fr] bg-white dark:bg-dark-user">
+          <div />
+          <CompactBoardMenu />
+          <div className="w-full relative">
+            <main className="border-x-[1px] dark:border-gray-500">{children}</main>
+            <ReloadBoard />
+          </div>
+          <SearchMenu />
+          <div />
         </div>
       ) : (
         <div className="grid grid-cols-[1fr_650px_1fr] bg-white dark:bg-dark-user">
